e802477246d4e252f52e5f4ae5ce7a5e7f4db7bd
Muntacir-Tamim <muntacirtamim202162@gmail.com>
2026-07-14 12:00:11 +0600

    feat(middleware): add validateRequest middleware with Zod integration

---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
+
+export const validateRequest = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
